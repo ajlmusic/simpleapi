fb37ccd5ae95c2e6b773e3cd47d3b3adfa0db9e2
Andrew Lewis <ajlmusic@gmail.com>
2022-11-11 20:14:05 -0600

    added jwt

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # simpleapi
+
+## Requirements
+- [Node.js](https://nodejs.org/en/)
+
+## Installation 
+
+```
+$ npm install
+
+```
+## Development Mode
+
+```
+$ npm run dev
+
+```
+
+## Production
+
+```
+$ npm run start
+
+```
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import cors from 'cors';
+/**
+ * Simple API with Express and JWT
+ * @author Andrew Lewis <andrew.lewis@nehetek.com>
+ * 
+ */
+
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 dotenv.config();
 
@@ -11,42 +19,67 @@ const port = process.env.PORT || 5500;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(process.env.TOKEN_SECRET);
+app.options("*", cors());
 
-app.options('*', cors());
+let randomNum = Math.random();
+let newUserID = crypto.randomBytes(16).toString("hex"); 
 
-app.get('/', (req, res) => {
-    let msg = {msg: 'Welcome to the Files API'}
-    res.json(msg);
+// TOKEN Generation and Verification
+
+// generate token
+function generateAccessToken(username) {
+  return jwt.sign({user: username}, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+// verify token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
+// endpoints
+app.get("/", (req, res) => {
+  let msg = { msg: "Welcome to the Files API" };
+  const token = generateAccessToken(newUserID);
+
+  res.json({ msg, token });
 });
 
-
-app.get('/categories', (req, res) => {
-    let categories = [
-        {id: 1, name: 'books'},
-        {id: 2, name: 'colleges'},
-        {id: 3, name: 'events'},
-        {id: 4, name: 'locations'}
-    ]
-    res.json(categories);
+app.get("/categories", (req, res) => {
+  let categories = [
+    { id: 1, name: "books" },
+    { id: 2, name: "colleges" },
+    { id: 3, name: "events" },
+    { id: 4, name: "locations" },
+  ];
+  res.json(categories);
 });
 
-app.get('/files', function(req, res){
-    let options = {
-        root: path.join(__dirname)
-    };
-     console.log(__dirname);
-    let fileName = 'welcome.pdf';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
+app.get("/files", function (req, res) {
+  let options = {
+    root: path.join(__dirname),
+  };
+  console.log(__dirname);
+  let fileName = "welcome.pdf";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
 });
-
-
-
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
