be84f8fab8f3b946af2f0452f058fba2177e03f6
Andrew Lewis <ajlmusic@gmail.com>
2022-11-13 08:01:55 -0600

    added uuid

--- src/middleware/generateUserId.js
+++ src/middleware/generateUserId.js
@@ -1,3 +1,7 @@
 import crypto from 'crypto';
+import {v4 as uuidv4} from 'uuid';
 
-export const newUserID = crypto.randomBytes(16).toString("hex");
+export const newUserID = uuidv4();
+
+export const randonString = crypto.randomBytes(16).toString("hex");
+
